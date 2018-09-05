6b57ef27f24cb13931ae75d9adad9666c61e7807
samzhuwj <samzhuwj@gmail.com>
2018-09-05 18:49:03 +0800

    the pimp idiom

---
+++ the_pimp_idiom.cpp
@@ -0,0 +1,36 @@
+// remove compilation dependencies on internal class implementations and improve compile times
+#include <memory>
+
+class foo
+{
+public:
+  foo();
+  ~foo();
+  foo(foo&&);
+  foo& operator=(foo&&);
+
+private:
+  class impl;
+  std::unique_ptr<impl> pimpl;
+};
+
+class foo::impl
+{
+public:
+  void do_internal_work()
+  {
+    internal_data = 5;
+  }
+
+private:
+  int internal_data = 0;
+};
+
+foo::foo() : pimpl{std::make_unique<impl>()}
+{
+  pimpl->do_internal_work();
+}
+
+foo::~foo() = default;
+foo::foo(foo&&) = default;
+foo& foo::operator=(foo&&) = default;
